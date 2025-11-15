d4c4d952accda013f9b9a076d935de5742348d4a
snail-boi <willemse.jens@gmail.com>
2025-11-15 23:54:13 +0100

    just comments

--- phone utils/App.xaml.cs
+++ phone utils/App.xaml.cs
@@ -7,6 +7,14 @@ namespace phone_utils
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+
+    // i will be moving out of beta soon so this versioning scheme will change, just a heads up
+    // also means we will need an extra version in between to go from beta to stable
+    // also i will almost review the entire codebase to optimize it and make it cleaner
+    // also the app will get a new name
+    //path's will also change to reflect the new name
+    // configs will now be found in snail-boi/new-app-name/ to provide compatibility with future apps and projects
+    // also the repo name will change
     public partial class App : Application
     {
         public static readonly string CurrentVersion = "v1.2-beta18";
